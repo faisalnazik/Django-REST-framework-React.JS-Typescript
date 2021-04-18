2ca34857ee6e9aa6640fd4ea72b169147d327913
Faisal Nazik <faisalnazik@gmail.com>
2021-04-18 09:35:26 +0500

    updated dependencies

--- Backend/config/urls.py
+++ Backend/config/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from django.conf import settings
+from django.conf.urls.static import static
+
+from accounts.api import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
