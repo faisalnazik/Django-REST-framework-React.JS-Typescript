6be6f14add13d61bc6c572d26e9637e8b77ec145
Faisal Nazik <faisalnazik@gmail.com>
2021-04-20 20:24:39 +0500

    20APRIL2021-01

---
+++ FrontEnd/reactapp-ts/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import { AppContainer } from "./styles"
+
+function App() {
+  return (
+    <AppContainer>
+      Data will go here
+    </AppContainer>
+  );
+}
+
+export default App;
---
+++ FrontEnd/reactapp-ts/src/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const AppContainer = styled.div`
+align-items: flex-start;
+background-color: #3179ba;
+display: flex;
+flex-direction: row;
+height: 100%;
+padding: 20px;
+width: 100%;
+`
