f865e29b13ecab7ca1db81c322af6fd06e334a33
Faisal Nazik <faisalnazik@gmail.com>
2021-04-26 21:29:48 +0500

    basic styles and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
+
+import { Column } from "./components/Column";
+import { Card } from "./components/Card";
 import "./App.css";
 import { AppContainer } from "./styles";
 
 function App() {
-  return <AppContainer>Data Will Go Here</AppContainer>;
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+    </AppContainer>
+  );
 }
 
 export default App;
